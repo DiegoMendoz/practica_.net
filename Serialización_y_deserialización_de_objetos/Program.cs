434896d0868ba2b36e59eef4ba5fbeec3e4c902c
Diego Mendoza <daygon2001@gmail.com>
2025-11-02 22:25:46 -0600

    practica de serealizacion y deserealizacion en json ademas de asegurar datos nulos

---
+++ Serialización_y_deserialización_de_objetos/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json;
+
+var hector = new People()
+{
+    Name = "Hector",
+    Age = 36,
+};
+string json = JsonSerializer.Serialize(hector);
+Console.WriteLine(json);
+
+string myJson = @"
+{""Name"":""Juan""
+,""Age"":36
+}";
+
+People? juan = JsonSerializer.Deserialize<People>(myJson);
+Console.WriteLine(juan?.Name);
+Console.WriteLine(juan?.Age);
+public class People
+{
+    public string? Name { get; set; }
+    public decimal Age { get; set; }
+}
