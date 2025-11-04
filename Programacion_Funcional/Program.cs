0d613192785a2f0a068eaa96c85d3e42a6b9d8c3
Diego Mendoza <daygon2001@gmail.com>
2025-11-03 18:10:19 -0600

    Programacion funcional

---
+++ Programacion_Funcional/Program.cs
@@ -0,0 +1,15 @@
+﻿var show = Show;
+
+Some(show,"Hola como estas");
+
+string Show(string message)
+{
+    return message.ToUpper();
+}
+
+void Some(Func<string,string> fn, string message)
+{
+    Console.WriteLine("Hace algo al inicio");
+    fn(message);
+    Console.WriteLine("hace algo al final");
+}
