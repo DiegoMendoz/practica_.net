51f9350f229f6d0c835bb5917124081f2cd3f75c
Diego Mendoza <daygon2001@gmail.com>
2025-11-03 20:00:25 -0600

    Linq

---
+++ LINQ/Program.cs
@@ -0,0 +1,13 @@
+﻿var names = new List<string>()
+{
+    "Hector","Francisco","Ana","Hugo","Pedro"
+};
+var namesResult = from n in names
+                  where n.Length>3 && n.Length<5
+                  orderby n descending
+                  select n;
+
+foreach (var name in namesResult)
+{
+    Console.WriteLine(name);
+}
